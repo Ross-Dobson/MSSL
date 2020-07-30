0b6793c1eb283c737a2a4f7fe93683610a776003
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-07-30 17:22:34 +0100

    Now downloads and concats entire year of data

--- SolarWindImport.py
+++ SolarWindImport.py
@@ -1,7 +1,6 @@
 import urllib.request
 import datetime
 import pathlib
-import sys
 
 import pandas as pd
 import numpy as np
@@ -25,16 +24,14 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
     Returns:
 
     Data -  DataFrame
-    
+
     Author: Andy Smith
-    Updated: Ross Dobson July 2020
-    
+    Updated: Ross Dobson, July 2020
+
     """
 
     Year = datetime.datetime.strftime(StartDateTime, '%Y')
-    # DOY = datetime.strftime(StartDateTime, '%j')
     Month = datetime.datetime.strftime(StartDateTime, '%m')
-    # DayOfMonth = datetime.strftime(StartDateTime, '%d')
 
     # See https://spdf.gsfc.nasa.gov/pub/data/omni/high_res_omni/hroformat.txt
     Header = ['Year', 'Day', 'Hour', 'Minute', 'B_IMF_ScID', 'Plasma_ScID',
@@ -53,11 +50,12 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
     FNameLocal = FDirLocal / ('OMNI_1min_'+Year+Month+'.asc')
 
     try:
+        # headers NOT in data, passed in via 'names' parameter instead
         Data = pd.read_csv(FNameLocal, sep='\s+', names=Header, header=None)
         print('Local data found at', FNameLocal)
 
         Data['DateTime'] = Data.apply(lambda row: datetime.datetime(int(row.Year), 1, 1)+datetime.timedelta(days=int(row.Day) - 1)+datetime.timedelta(seconds=row.Hour*60*60+row.Minute*60), axis=1)
-        
+
     # FileNotFoundError from pd.read_csv means we need to download.
     except FileNotFoundError:
 
@@ -70,23 +68,23 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
         print('Done. Downloading data to: ', FNameLocal)
         urllib.request.urlretrieve(FNameWeb, FNameLocal)  # Saves to FNameLocal
         print('Data downloaded.')
-
+        # headers NOT in data, passed in via 'names' parameter instead
         Data = pd.read_csv(FNameLocal, sep='\s+', names=Header, header=None)
         Data['DateTime'] = Data.apply(lambda row: datetime.datetime(int(row.Year), 1, 1)+datetime.timedelta(days=int(row.Day) - 1)+datetime.timedelta(seconds=row.Hour*60*60+row.Minute*60), axis=1)
 
-        # Select the data within our range - failsafe?
-        Data = Data[(Data.DateTime >= StartDateTime)
-                    & (Data.DateTime <= EndDateTime)]
+    # Select the data within our range - failsafe?
+    Data = Data[(Data.DateTime >= StartDateTime)
+                & (Data.DateTime <= EndDateTime)]
 
-        # Bodge any borked data with NaN.
-        Data = Data.replace(99.99, np.nan)
-        Data = Data.replace(999.9, np.nan)
-        Data = Data.replace(999.99, np.nan)
-        Data = Data.replace(9999.99, np.nan)
-        Data = Data.replace(99999.9, np.nan)
-        Data = Data.replace(9999999., np.nan)
+    # Bodge any borked data with NaN.
+    Data = Data.replace(99.99, np.nan)
+    Data = Data.replace(999.9, np.nan)
+    Data = Data.replace(999.99, np.nan)
+    Data = Data.replace(9999.99, np.nan)
+    Data = Data.replace(99999.9, np.nan)
+    Data = Data.replace(9999999., np.nan)
 
-    # Make DateTime the index of the dataframe
+    # Make DateTime the index of the dataframe - ie the row "headers"
     Data.index = Data['DateTime']
 
     # We defined this up the top, remember? Passed in.
@@ -98,12 +96,55 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
 
 
 def main():
-    # I'll probably have to change this for looping over a whole year, but
-    # thats future me's problem
-    startDT = datetime.datetime(2003, 10, 1)
-    endDT = datetime.datetime(2003, 10, 31)
-    data = import_omni_month(startDT, endDT)
-    print(data)
+
+    # Let's start with an empty dataframe, but with our headers
+    data = pd.DataFrame(columns=['Year', 'Day', 'Hour', 'Minute', 'B_IMF_ScID',
+                                 'Plasma_ScID', 'IMFAvPoints',
+                                 'PlasmaAvPoints', 'PercentInterp',
+                                 'Timeshift', 'RMSTimeshift',
+                                 'RMSPhaseFrontNormal', 'TimeBetweenObs', 'B',
+                                 'B_X_GSM', 'B_Y_GSE', 'B_Z_GSE',
+                                 'B_Y_GSM', 'B_Z_GSM', 'RMSBScalar',
+                                 'RMSFieldVector', 'V', 'V_X_GSE', 'V_Y_GSE',
+                                 'V_Z_GSE', 'n_p', 'T', 'P', 'E', 'Beta',
+                                 'AlfvenMachff', 'X_SC_GSE', 'Y_SC_GSE',
+                                 'Z_SC_GSE', 'X_BSN_GSE', 'Y_BSN_GSE',
+                                 'Z_BSN_GSE', 'AE', 'AL', 'AU', 'SymD', 'SymH',
+                                 'AsyD', 'AsyH', 'PCN', 'MagnetosonicMach',
+                                 '10MeVProton', '30MeVProton', '60MeVProton'])
+
+    year = 2003
+
+    # Check if it's a leap year or not
+    leap_year = False
+    if(year % 4 != 0):
+        leap_year = False
+    elif(year % 100 == 0):
+        if(year % 400 == 0):
+            leap_year = True
+        else:
+            leap_year = False
+    else:
+        leap_year = True
+
+    # List end day for each month so we can cycle through
+    month_end_dates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if(leap_year):
+        month_end_dates[1] = 29
+
+    # Blank array to store our DateTime objects in
+    startDT = []
+    endDT = []
+
+    for i in range(0, 12):
+        startDT.append(datetime.datetime(year, (i+1), 1))
+        endDT.append(datetime.datetime(year, (i+1), month_end_dates[i],
+                                       23, 59, 59))
+        this_month_data = import_omni_month(startDT[i], endDT[i], Resolution='1min', Columns='All')
+        # print(this_month_data)
+        data = pd.concat([data, this_month_data])  # add it into the main DF
+
+        print(data)
 
 
 main()
