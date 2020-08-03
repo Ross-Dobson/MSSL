88b602a97aeaa040bd1f018e9e11c9d0118d3256
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-08-03 20:56:21 +0100

    plots entire year, and does october seperately too

--- SolarWindImport.py
+++ SolarWindImport.py
@@ -4,6 +4,7 @@ import pathlib
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt  # ignore the 'unused' error, pandas needs it
 
 
 def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
@@ -54,7 +55,13 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
         Data = pd.read_csv(FNameLocal, sep='\s+', names=Header, header=None)
         print('Local data found at', FNameLocal)
 
-        Data['DateTime'] = Data.apply(lambda row: datetime.datetime(int(row.Year), 1, 1)+datetime.timedelta(days=int(row.Day) - 1)+datetime.timedelta(seconds=row.Hour*60*60+row.Minute*60), axis=1)
+        Data['DateTime'] = Data.apply(
+            lambda row:
+            datetime.datetime(int(row.Year), 1, 1)
+            + datetime.timedelta(
+                days=int(row.Day) - 1)
+            + datetime.timedelta(seconds=row.Hour*60*60 + row.Minute*60),
+            axis=1)
 
     # FileNotFoundError from pd.read_csv means we need to download.
     except FileNotFoundError:
@@ -63,20 +70,29 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
               + 'downloading from https://spdf.sci.gsfc.nasa.gov')
         FNameWeb = ('https://spdf.sci.gsfc.nasa.gov/pub/data/omni/' +
                     'high_res_omni/monthly_1min/omni_min'+Year+Month+'.asc')
+
         print('Creating local directory (skips if already exists)')
         pathlib.Path(FDirLocal).mkdir(exist_ok=True)
+
         print('Done. Downloading data to: ', FNameLocal)
         urllib.request.urlretrieve(FNameWeb, FNameLocal)  # Saves to FNameLocal
         print('Data downloaded.')
+
         # headers NOT in data, passed in via 'names' parameter instead
         Data = pd.read_csv(FNameLocal, sep='\s+', names=Header, header=None)
-        Data['DateTime'] = Data.apply(lambda row: datetime.datetime(int(row.Year), 1, 1)+datetime.timedelta(days=int(row.Day) - 1)+datetime.timedelta(seconds=row.Hour*60*60+row.Minute*60), axis=1)
 
-    # Select the data within our range - failsafe?
+        Data['DateTime'] = Data.apply(
+            lambda row:
+            datetime.datetime(int(row.Year), 1, 1)
+            + datetime.timedelta(days=int(row.Day) - 1)
+            + datetime.timedelta(seconds=row.Hour*60*60+row.Minute*60),
+            axis=1)
+
+    # Select the data within our range
     Data = Data[(Data.DateTime >= StartDateTime)
                 & (Data.DateTime <= EndDateTime)]
 
-    # Bodge any borked data with NaN.
+    # Bodge any borked data with NaN, as pandas knows not to plot this
     Data = Data.replace(99.99, np.nan)
     Data = Data.replace(999.9, np.nan)
     Data = Data.replace(999.99, np.nan)
@@ -84,12 +100,11 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
     Data = Data.replace(99999.9, np.nan)
     Data = Data.replace(9999999., np.nan)
 
-    # Make DateTime the index of the dataframe - ie the row "headers"
+    # Make DateTime the index of the dataframe - ie the row labels
     Data.index = Data['DateTime']
 
     # We defined this up the top, remember? Passed in.
     if Columns != 'All':
-
         Data = Data[Columns]
 
     return Data
@@ -98,20 +113,21 @@ def import_omni_month(StartDateTime, EndDateTime, Resolution='1min',
 def main():
 
     # Let's start with an empty dataframe, but with our headers
-    data = pd.DataFrame(columns=['Year', 'Day', 'Hour', 'Minute', 'B_IMF_ScID',
-                                 'Plasma_ScID', 'IMFAvPoints',
-                                 'PlasmaAvPoints', 'PercentInterp',
-                                 'Timeshift', 'RMSTimeshift',
-                                 'RMSPhaseFrontNormal', 'TimeBetweenObs', 'B',
-                                 'B_X_GSM', 'B_Y_GSE', 'B_Z_GSE',
-                                 'B_Y_GSM', 'B_Z_GSM', 'RMSBScalar',
-                                 'RMSFieldVector', 'V', 'V_X_GSE', 'V_Y_GSE',
-                                 'V_Z_GSE', 'n_p', 'T', 'P', 'E', 'Beta',
-                                 'AlfvenMachff', 'X_SC_GSE', 'Y_SC_GSE',
-                                 'Z_SC_GSE', 'X_BSN_GSE', 'Y_BSN_GSE',
-                                 'Z_BSN_GSE', 'AE', 'AL', 'AU', 'SymD', 'SymH',
-                                 'AsyD', 'AsyH', 'PCN', 'MagnetosonicMach',
-                                 '10MeVProton', '30MeVProton', '60MeVProton'])
+    omni_headers = ['Year', 'Day', 'Hour', 'Minute', 'B_IMF_ScID',
+                    'Plasma_ScID', 'IMFAvPoints', 'PlasmaAvPoints',
+                    'PercentInterp', 'Timeshift', 'RMSTimeshift',
+                    'RMSPhaseFrontNormal', 'TimeBetweenObs', 'B', 'B_X_GSM',
+                    'B_Y_GSE', 'B_Z_GSE', 'B_Y_GSM', 'B_Z_GSM', 'RMSBScalar',
+                    'RMSFieldVector', 'V', 'V_X_GSE', 'V_Y_GSE', 'V_Z_GSE',
+                    'n_p', 'T', 'P', 'E', 'Beta', 'AlfvenMachff', 'X_SC_GSE',
+                    'Y_SC_GSE', 'Z_SC_GSE', 'X_BSN_GSE', 'Y_BSN_GSE',
+                    'Z_BSN_GSE', 'AE', 'AL', 'AU', 'SymD', 'SymH', 'AsyD',
+                    'AsyH', 'PCN', 'MagnetosonicMach', '10MeVProton',
+                    '30MeVProton', '60MeVProton']
+
+    # make two blank dataframes - one for the year, one for our data
+    data = pd.DataFrame(columns=omni_headers)
+    october_data = pd.DataFrame(columns=omni_headers)
 
     year = 2003
 
@@ -140,11 +156,20 @@ def main():
         startDT.append(datetime.datetime(year, (i+1), 1))
         endDT.append(datetime.datetime(year, (i+1), month_end_dates[i],
                                        23, 59, 59))
-        this_month_data = import_omni_month(startDT[i], endDT[i], Resolution='1min', Columns='All')
+        this_month_data = import_omni_month(
+            startDT[i], endDT[i], Resolution='1min', Columns='All')
+        if (i == 9):
+            october_data = this_month_data
         # print(this_month_data)
         data = pd.concat([data, this_month_data])  # add it into the main DF
 
-        print(data)
+    print(data)
+    plot_vals = ['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'AL', 'P', 'V']
+    for val in plot_vals:
+        year_title = val + ' in 2003'
+        october_title = val + ' in October 2003'
+        data.plot(x='DateTime', y=val, title=year_title)
+        october_data.plot(x='DateTime', y=val, title=october_title)
 
 
 main()
