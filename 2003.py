1081aabb9083edc061d01434ae9ce19800d0181a
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-08-10 16:40:50 +0100

    data transformed more correctly, histograms ARE correct

--- 2003.py
+++ 2003.py
@@ -2,10 +2,12 @@ import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 # remember to run 'matplotlib tk' in the interpreter
 
-from SolarWindImport import import_omni_month, import_omni_year
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from SolarWindImport import import_omni_year
 
 
 def main():
@@ -15,33 +17,36 @@ def main():
     year = 2003
     df_2003 = import_omni_year(year)
 
-    pkl_dir = pathlib.Path('Data/OMNI/pickles')
-    oct_nov_pkl_path = pkl_dir / 'oct_nov_2003.pkl'
+    # pkl_dir = pathlib.Path('Data/OMNI/pickles')
+    # oct_nov_pkl_path = pkl_dir / 'oct_nov_2003.pkl'
 
-    try:
-        df_oct_nov_2003 = pd.read_pickle((oct_nov_pkl_path))
-        print("Found the pickle for October and November 2003.")
+    # try:
+    #     df_oct_nov_2003 = pd.read_pickle((oct_nov_pkl_path))
+    #     print("Found the pickle for October and November 2003.")
 
-    except FileNotFoundError:
-        print("Couldn't find the pickle for October and November 2003."
-              + "-> Generating one from the .asc files.")
+    # except FileNotFoundError:
+    #     print("Couldn't find the pickle for October and November 2003."
+    #           + "-> Generating one from the .asc files.")
 
-        print('Creating local directory at ', pkl_dir,
-              ' (if it doesn\'t already exist):')
-        pathlib.Path(pkl_dir).mkdir(exist_ok=True)
-        print("Done.")
+    #     print('Creating local directory at ', pkl_dir,
+    #           ' (if it doesn\'t already exist):')
+    #     pathlib.Path(pkl_dir).mkdir(exist_ok=True)
+    #     print("Done.")
 
-        df_oct_2003 = import_omni_month(2003, 10)
-        df_nov_2003 = import_omni_month(2003, 11)
-        df_oct_nov_2003 = pd.concat([df_oct_2003, df_nov_2003])
-        df_oct_nov_2003.to_pickle(oct_nov_pkl_path)
+    #     df_oct_2003 = import_omni_month(2003, 10)
+    #     df_nov_2003 = import_omni_month(2003, 11)
+    #     df_oct_nov_2003 = pd.concat([df_oct_2003, df_nov_2003])
+    #     df_oct_nov_2003.to_pickle(oct_nov_pkl_path)
 
+    print("DF 2003:")
     print(df_2003)
 
-    # plotting
+    # PLOTTING
+
+    hist_dir = pathlib.Path('Figures')
+    pathlib.Path(hist_dir).mkdir(exist_ok=True)
 
     plot_vals = ['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'AL', 'P', 'V']
-
     # for val in plot_vals:
     #     year_title = str(val) + ' in ' + str(year)
     #     df_2003.plot(x='DateTime', y=val, title=year_title)
@@ -52,37 +57,62 @@ def main():
     print("\nCorrelation matrix for 2003\n")
     corr_2003 = df_2003[plot_vals].corr()
     print(corr_2003)
+    # print("\nCorrelation matrix for October & November 2003\n")
+    # corr_oct_nov_2003 = df_oct_nov_2003[plot_vals].corr()
+    # print(corr_oct_nov_2003)
 
-    print("\nCorrelation matrix for October & November 2003\n")
-    corr_oct_nov_2003 = df_oct_nov_2003[plot_vals].corr()
-    print(corr_oct_nov_2003)
+    # SCALER/TRANSFORM THE DATA
 
-    csv_dir = pathlib.Path('Data/OMNI/corr')
-    pathlib.Path(csv_dir).mkdir(exist_ok=True)
-    csv_path = csv_dir / 'corr_2003.csv'
-    corr_2003.to_csv(csv_path)
+    # the features we care about
+    model_vals = ['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'P', 'V']
 
-    csv_oct_nov_2003_path = csv_dir / 'corr_oct_nov_2003.csv'
-    corr_oct_nov_2003.to_csv(csv_oct_nov_2003_path)
+    # now get just that data. Get AL seperately, as it's the output/target here
+    df2 = df_2003[model_vals]
+    AL = df_2003['AL']
 
-    # 2: STANDARDIZE THE DATA
+    print("\nDF2:")
+    print(df2)
+    df3 = df2.to_numpy()
+    AL2 = AL.to_numpy()
+    AL3 = AL2.reshape(-1, 1)
 
-    # strip headers -> raw data only. Seperate AL as that's the output/target
-    data1 = df_2003[['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'P', 'V']].to_numpy()
-    AL1 = df_2003['AL'].to_numpy()
-
-    # Standardize the data array
+    # scale the main features
     scaler = StandardScaler()
-    scaler.fit(data1)
-    data2 = scaler.transform(data1)
+    scaler = scaler.fit(df3)
+    df4 = scaler.transform(df3)
+    df5 = df4.T
 
-    # let's check the histograms
-    data3 = np.transpose(data2)
-    for i, param in enumerate(['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'P', 'V']):
+    # scale AL
+    scaler2 = StandardScaler()
+    scaler2 = scaler2.fit(AL3)
+    AL4 = scaler2.transform(AL3)
+
+    # fig, axs = plt.subplots(2, 3, tight_layout=True,
+    #                        sharex=False, sharey=False)
+    # fig.suptitle('Histograms after StandardScaler')
+
+    # plot_vals = np.array(model_vals).reshape(2, 3)
+
+    # for (i, j), param in np.ndenumerate(plot_vals):
+    #     axs[i, j].hist(df5[i, j], bins=10)
+    #     axs[i, j].set_title(param)
+
+    for i, param in enumerate(model_vals):
         plt.figure()
-        plt.hist(data3[i], bins=30)
-        plt.title('Histogram of ' + param)
-        plt.show()
+        plt.hist(df5[i], bins=36)
+        plt.title('Histogram of '+param+' after StandardScaler')
+
+    plt.figure()
+    plt.hist(AL4, bins=36)
+    plt.title('Histogram of AL after StandardScaler')
+
+    # # TRAIN TEST SPLIT
+
+    # # split the data into two equal chunks, for training and testing
+    # X_train, X_test, y_train, y_test = train_test_split(
+    #     df3, AL2, test_size=0.33, random_state=47)
+
+    # # now, let's do some cross-validation
 
 
 main()
