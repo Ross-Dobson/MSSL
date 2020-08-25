61c4ed27931151fb9d85d56205b9bcb340ea7ad9
Ross Dobson <56548895+Ross-Dobson@users.noreply.github.com>
2020-08-25 12:53:57 +0100

    tidied up, commit for teams call. TODO: discretize AL

--- 2003.py
+++ 2003.py
@@ -47,8 +47,8 @@ def main():
     #     df_oct_nov_2003.to_pickle(oct_nov_pkl_path)  # store for future use
 
     print("All 2003 data has been loaded.")
-    print("\n2003 data:")
-    print(data_2003)
+    # print("\n2003 data:")
+    # print(data_2003)
 
     # ---------------------------------------------------------------
     # PLOTTING THE PARAMETERS
@@ -64,7 +64,7 @@ def main():
     # ---------------------------------------------------------------
     # CORRELATION MATRIX BEFORE SCALER
 
-    print("\nCorrelation matrix for 2003:")
+    print("\nCorrelation matrix before standardization:\n")
     corr_2003 = data_2003[plot_vals].corr()
     print(corr_2003)
 
@@ -78,32 +78,7 @@ def main():
     # The features we care about
     model_vals = ['B_X_GSM', 'B_Y_GSM', 'B_Z_GSM', 'n_p', 'P', 'V']
 
-    # Now get just that data. Get AL seperately, as it's the output/target here
-    # This also makes it more futureproof for changing the target
-    # df = df_2003[model_vals]
-    # AL = df_2003['AL']
-
-    # print("\nDF2:")
-    # print(df2)
-
-    # # strip the labels and column headers
-    # df3 = df2.to_numpy()  # we don't need to .T, each column is a feature
-    # AL2 = AL.to_numpy()
-    # # print(df3.shape)
-    # AL3 = AL2.reshape(-1, 1)  # however, this needs to be .T'd into a column
-
-    # # scale the main features
-    # scaler = StandardScaler()
-    # scaler = scaler.fit(df3)
-    # df4 = scaler.transform(df3)
-    # df5 = df4.T  # make each ROW, not column, a feature, to plot histogram
-
-    # # scale AL
-    # scaler2 = StandardScaler()
-    # scaler2 = scaler2.fit(AL3)
-    # AL4 = scaler2.transform(AL3)
-    # AL5 = AL4.T
-
+    # get just that data
     df_2003 = data_2003[model_vals]
     df_2003_cols = df_2003.columns  # column labels
     df_2003_index = df_2003.index  # row labels
@@ -115,11 +90,12 @@ def main():
     scaler = StandardScaler()
     scaler = scaler.fit(df_2003)
     arr_2003_scaled = scaler.transform(df_2003)
+
     # need to add it back into a DF
     df_2003 = pd.DataFrame(arr_2003_scaled,
                            columns=df_2003_cols, index=df_2003_index)
-    print("\nDF 2003 after scaling:\n")
-    print(df_2003)
+    # print("\nDF 2003 after scaling:\n")
+    # print(df_2003)
 
     # scale AL
     scaler2 = StandardScaler()
@@ -127,26 +103,28 @@ def main():
     arr_AL = arr_AL.reshape(-1, 1)
     scaler2 = scaler2.fit(arr_AL)
     arr_AL_scaled = scaler2.transform(arr_AL)
+
+    # need to add it back into a DF
     df_AL = pd.DataFrame(arr_AL_scaled, columns=['AL'], index=df_AL_index)
 
-    print("\nAL after scaling\n")
-    print(df_AL)
+    # print("\nAL after scaling\n")
+    # print(df_AL)
 
     # ---------------------------------------------------------------
     # CORR AFTER STANDARDISATION
     df_2003.insert(4, "AL", arr_AL_scaled)
-    print("\nDEBUG\n")
-    print(df_2003)
+    print("\nCorr after standardization:\n")
     print(df_2003.corr())
 
     # ---------------------------------------------------------------
     # INTERPOLATING GAPS
     # resample the data into one minute chunks
 
+    # unsure as to why we do this resample first
     df_resampled = df_2003.resample(
         '1T', loffset=datetime.timedelta(seconds=30.)).mean()
     df_2003 = df_resampled.interpolate(method='linear', limit=15)
-    # I've tested, this does work. Can use np.isnan on each value to check
+    # I've tested, dropna does work. Can use np.isnan on each value to check
 
     # ---------------------------------------------------------------
     # REMOVING NaN VALUES
@@ -154,21 +132,25 @@ def main():
     df_2003.dropna(axis='index', how='any', inplace=True)
     # to check this has worked, notice the last 30 minutes on Dec 31 are gone.
     # previously, these were riddled with NaNs.
-    
+
     # ---------------------------------------------------------------
     # PLOT HISTOGRAMS:
 
     # hist_dir = pathlib.Path('Figures')
     # pathlib.Path(hist_dir).mkdir(exist_ok=True)
 
-    # for i, param in enumerate(model_vals):
+    # for i, param in enumerate(plot_vals):
     #     plt.figure()
-    #     plt.hist(df.T[i], bins=36)
+    #     plt.hist(df_2003[param], bins=35)
     #     plt.title('Histogram of '+param+' after StandardScaler')
 
-    # plt.figure()
-    # plt.hist(AL, bins=36)
-    # plt.title('Histogram of AL after StandardScaler')
+    # ---------------------------------------------------------------
+    # DISCRETIZE AL
+    df_AL = df_2003['AL']
+    print("\nDiscretizing AL:\n")
+
+    # df_AL.rolling(30).min()
+    # TODO: need to align to right - use shift as per stack overflow?
 
     # ---------------------------------------------------------------
     # TRAIN TEST SPLIT
